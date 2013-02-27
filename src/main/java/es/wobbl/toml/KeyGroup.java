b5cbf3913c0030416b1a440c1a0696404e3c7b19
Matthias Schuetz <mschuetz@gmail.com>
2013-02-27 23:55:05 +0100

    simplified, guarantee non null

--- src/main/java/es/wobbl/toml/KeyGroup.java
+++ src/main/java/es/wobbl/toml/KeyGroup.java
@@ -12,7 +12,7 @@ public class KeyGroup {
 	private final boolean root;
 
 	public KeyGroup(String name, boolean root) {
-		this.name = name;
+		this.name = Preconditions.checkNotNull(name);
 		this.root = root;
 	}
 
@@ -24,28 +24,22 @@ public class KeyGroup {
 		return name;
 	}
 
-	public Object getMember(String key) {
-		if (key.contains("."))
-			return getMemberRecursive(key);
-		else
-			return members.get(key);
-	}
-
-	private Object getMemberRecursive(String path) {
-		Preconditions.checkArgument(path.contains("."));
+	public Object getMember(String path) {
 		KeyGroup cur = this;
 		int i = 0;
 		final String[] parts = path.split("\\.");
 		for (final String key : parts) {
 			if (i < parts.length - 1) {
-				cur = (KeyGroup) cur.getMember(key);
+				cur = (KeyGroup) cur.members.get(key);
 				Preconditions.checkArgument(cur != null, "child node doesn't exist: %s", path);
 			} else {
-				return cur.getMember(key);
+				Object obj = cur.members.get(key);
+				Preconditions.checkArgument(obj != null, "child node doesn't exist: %s", path);
+				return obj;
 			}
 			i++;
 		}
-		return null;
+		throw new IllegalStateException("unreachable");
 	}
 
 	public void addMember(String key, Object value) {
